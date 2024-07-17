faec99f939d4487287f8152949c81e16bd351802
Aryo Aditya <aryoaditya2000@gmail.com>
2024-07-17 22:25:18 +0700

    feat: show items to the render list

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Form from "./components/Form"
 import Checklist from "./components/Checklist"
 import Stats from "./components/Stats"
 
+const listItems = [
+  {id: 1, title: "Eat", done: false},
+  {id: 2, title: "Sleep", done: true},
+];
+
 function App() {
   return (
     <div className="app">
       <Logo />
       <Form />
-      <Checklist />
+      <Checklist listItems={listItems}/>
       <Stats />
     </div>
   );
--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -1,12 +1,24 @@
-function Checklist() {
+function Checklist({listItems}) {
     return (
         <div className="list">
-        <ul>
-            <li>Makan</li>
-            <li>Tidur</li>
-        </ul>
+            <ul>
+                {listItems.map((item) => (
+                    <Item item={item} key={item.id}/>
+                ))}
+            </ul>
+            
         </div>
     );
+}
+
+function Item({item}) {
+    return (
+        <li key={item.id}>
+            <input type="checkbox" />
+            <span style={{ textDecoration: item.done ? "line-through" : "" }}> {item.title} </span> 
+            <button> ❌ </button>
+        </li>
+    )
 }
 
 export default Checklist;
